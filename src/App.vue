28634095f345d4ef6b0bcc7b61374ab29c326226
nebbles <nebbles@protonmail.com>
2019-09-10 15:14:31 -0600

    nav-drawer toggle button added to app-bar, prettier lint fixes

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,26 @@
 <template>
   <v-app>
-  <v-navigation-drawer app>
-  </v-navigation-drawer>
-  <v-app-bar app>
-    <v-btn class="ma-2" to="/">Home</v-btn>
-    <v-btn class="ma-2" to="/about">About</v-btn>
-  </v-app-bar>
-  <v-content>
-    <v-container fluid>
-      <router-view></router-view>
-    </v-container>
-  </v-content>
-  <v-footer app> 
-  </v-footer>
-</v-app>
+    <v-navigation-drawer app v-model="drawer" absolute temporary>
+    </v-navigation-drawer>
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-btn class="ma-2" to="/">Home</v-btn>
+      <v-btn class="ma-2" to="/about">About</v-btn>
+    </v-app-bar>
+    <v-content>
+      <v-container fluid>
+        <router-view></router-view>
+      </v-container>
+    </v-content>
+    <v-footer app></v-footer>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
-
 export default {
   name: "App",
-  components: {
-    HelloWorld,
-  },
   data: () => ({
-    
-  }),
+    drawer: null
+  })
 };
 </script>
