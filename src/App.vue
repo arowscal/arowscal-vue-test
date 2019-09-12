eabb88c837a21fe10ab68f4a602cbbb3ac1d9b93
nebbles <nebbles@protonmail.com>
2019-09-12 11:07:59 -0600

    clipped nav-drawer and app-bar

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
 <template>
   <v-app>
-    <v-navigation-drawer app v-model="drawer" absolute temporary>
-    </v-navigation-drawer>
-    <v-app-bar app>
+    <v-navigation-drawer app v-model="drawer" clipped></v-navigation-drawer>
+    <v-app-bar app clipped-left>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-      <v-btn class="ma-2" to="/">Home</v-btn>
-      <v-btn class="ma-2" to="/about">About</v-btn>
+      <v-btn class="ma-2 justify-end" to="/">Home</v-btn>
+      <v-btn class="ma-2 justify-end" to="/about">About</v-btn>
     </v-app-bar>
     <v-content>
       <v-container fluid>
