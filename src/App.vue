61d63ebc366df383b4833547bd984399666c1e32
nebbles <nebbles@protonmail.com>
2019-09-12 13:12:17 -0600

    added v-spacer to app-bar

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <v-navigation-drawer app v-model="drawer" clipped></v-navigation-drawer>
     <v-app-bar app clipped-left>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-spacer></v-spacer>
       <v-btn class="ma-2 justify-end" to="/">Home</v-btn>
       <v-btn class="ma-2 justify-end" to="/about">About</v-btn>
     </v-app-bar>
